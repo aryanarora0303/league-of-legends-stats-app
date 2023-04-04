76afe3266be134bfe9f1358f5ef9c47a83b54ab1
Aryan Arora <aryanarora@Aryans-MacBook-Pro.local>
2023-04-04 16:16:59 -0700

    Extract Summoner, Champion spells and Summoner Items
    ChampionFull, Champion, Item, SummonerSpells data retrieved from DataDragon. Multiple endpoints to extract the data from, listed as alternative in getStaticData function.

--- lambda_server/index.js
+++ lambda_server/index.js
@@ -4,6 +4,44 @@ const LeagueJS = require('leaguejs');
 // Create LeagueJS client
 const leagueJs = new LeagueJS('RGAPI-8b4bdf52-7e65-4a9c-bf54-80d6215b9ea5', { PLATFORM_ID: 'na'});
 
+// Global Variables
+let summonerSpellData;
+let itemData;
+let championsData;
+
+// Retrives Static data from Data Dragon
+async function getStaticData() {
+    try {
+      console.log("Retrieving Static Data");
+      summonerSpellData = fs.readFileSync('./node_modules/leaguejs/StaticDataDefaultRoot/13.6.1/en_US/summoner.json', 'utf8');
+      summonerSpellData = JSON.parse(summonerSpellData);
+      summonerSpellData = summonerSpellData.data;
+      console.log(summonerSpellData);
+      // ALT:
+      // summonerSpellData = (await DataDragonHelper.gettingSummonerSpellsList()).data;
+      // summonerSpellData = (await axios.get(`http://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/summoner.json`)).data;
+  
+      itemData = fs.readFileSync('./node_modules/leaguejs/StaticDataDefaultRoot/13.6.1/en_US/item.json', 'utf8');
+      itemData = JSON.parse(itemData);
+      itemData = itemData.data;
+      console.log(itemData);
+      // ALT:
+      // itemData = (await DataDragonHelper.gettingItemList()).data;
+      // itemData = (await axios.get('https://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/item.json')).data;
+  
+      championsData = fs.readFileSync('./node_modules/leaguejs/StaticDataDefaultRoot/13.6.1/en_US/championFull.json', 'utf8');
+      championsData = JSON.parse(championsData);
+      championsData = championsData.data;
+      console.log(championsData);
+      // ALT:
+      // championsData = (NO DataDragonHelper Method to retrieve this data);
+      // championData = (await axios.get(`https://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/champion/${championName}.json`)).data.data;
+    } catch(error) {
+      console.error('Error retrieving static data:', error);
+      throw error;
+    }
+  }
+
 // Extracts and Formats match detials
 async function getMatchDetails(match, summoner) {
     try {
@@ -34,32 +72,69 @@ async function getMatchDetails(match, summoner) {
         let creepScorePerMin = (totalCreepScore / (gameDurationSeconds/60));
 
         console.log("MyParticipant Basic Info Retrieved");
+
+        // Lookup spells casted by champion
+        let championData = championsData[championName];
+        let championSpells = []
+        for (let spell of championData.spells) { // championData.spells format: [ Object, Object, ... ]
+            if (spell?.name) {
+                championSpells.push(spell.name);
+            }
+        }
+
+        console.log("MyParticipant championSpells Retrieved");
+
+        // Lookup spells casted by summoner
+        let summonerSpells = []
+        for (let key of Object.keys(summonerSpellData)) { // summonerSpellData format: { Object, Object, ... }
+            let spell = summonerSpellData[key];
+            if (spell?.name && spell?.id && ((spell.id == myParticipant.summoner1Id) || (spell.id == myParticipant.summoner2Id))) {
+                summonerSpells.push(spell.name);
+            } 
+        }
+
+        console.log("MyParticipant summonerSpells Retrieved");
+
+
+        // Lookup items used by summoner
+        let items = []
+        for (let key of itemIDs) { // itemIDs format: [ id, id, ... ]
+            let item = itemData[key];
+            if (item?.name) {
+                items.push(item.name);
+            }
+        }
+
+        console.log("MyParticipant items Retrieved");
     
         let matchData = {
             outcome,
             gameDurationSeconds,
             summonerName,
             summonerLevel,
+            summonerSpells,
             summonerPerks,
+            championSpells,
             championSpellCounts,
             totalKills,
             totalDeaths,
             totalAssists,
             kda,
+            items,
             championName,
             championLevel,
             totalCreepScore,
             creepScorePerMin
-          };
+        };
     
         console.log("MyParticipant Match Data Retrieved");
     
         return matchData;
     
-      } catch (error) {
+    } catch (error) {
         console.error('Error fetching match details:', error);
         throw error;
-      }
+    }
 }
 
 // Main Entry Point
