5ec74ec54735a676bf661e0b6b93d82a3cbc4f25
Aryan Arora <aryanarora@Aryans-MacBook-Pro.local>
2023-04-04 14:00:20 -0700

    Added boilerplate code for a Lambda server.
    Includes boilerplate code to test the function request, response cycle on AWS

--- lambda_server/index.js
+++ lambda_server/index.js
@@ -1,11 +1,26 @@
 // Imports
+const LeagueJS = require('leaguejs');
+
+// Create LeagueJS client
+const leagueJs = new LeagueJS('RGAPI-7a708be1-fbe1-4c64-bf13-a526a858e012', { PLATFORM_ID: 'na'});
+
+// Extracts and Formats match detials
+async function getMatchDetails() {}
 
 // Main Entry Point
-export const handler = async(event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+// Lambda Handler
+exports.handler = async (event) => {
+
+    console.log("Event", event);
+    let method = event?.requestContext?.http?.method;
+    let path = event?.requestContext?.http?.path;
+
+    if (method == "GET" && path == "/matchHistoryDetails") {
+        console.log("Requesting: ", method, path);
+        return { statusCode: 200, body: JSON.stringify('Hello from Lambda!') }
+    }
+    // else if { method == "" && path == "" }
+    else {
+        return { statusCode: 404, body: "404 Not Found" }
+    }
+}
